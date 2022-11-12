c0fbaf923f9a0fb9ae0551978e4c3256077665f2
Brynta2001 <110837963+Brynta2001@users.noreply.github.com>
2022-11-12 12:22:07 -0500

    Lectura de datos de surtidores de combustible

--- src/LectorDatos.java
+++ src/LectorDatos.java
@@ -1,3 +1,28 @@
-public class LectorDatos {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class LectorDatos {
+    public List leerDatos(String fecha) {
+        List consumoDiario = new ArrayList<String>();
+        String line = "";
+        String splitBy = ",";
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("/Lecturas/lecturas.csv"));
+            while ((line = br.readLine()) != null)   //returns a Boolean value
+            {
+                String[] datos = line.split(splitBy);    // use comma as separator
+                //System.out.println("Employee [First Name=" + employee[0] + ", Last Name=" + employee[1] + ", Designation=" + employee[2] + ", Contact=" + employee[3] + ", Salary= " + employee[4] + ", City= " + employee[5] +"]");
+                if(datos[0].equals(fecha)){
+                    consumoDiario.add(datos[1]);
+                    consumoDiario.add(datos[2]);
+                    consumoDiario.add(datos[3]);
+                }
+            }
+        }catch (IOException e){
+            System.out.println("No se encuentra el archivo de lecturas: " + e);
+        }
+        return consumoDiario;
+    }
 }
